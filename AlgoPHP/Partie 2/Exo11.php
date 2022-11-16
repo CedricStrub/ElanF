d6457185f5aea3a63d87fa02e7b5fce4baee97fe
ErgoNoMister <strubcedric@gmail.fr>
2022-11-16 15:19:08 +0100

    Exo 12

--- AlgoPHP/Partie 2/Exo11.php
+++ AlgoPHP/Partie 2/Exo11.php
@@ -1,4 +1,4 @@
-<h1>Exercice 3</h1>
+<h1>Exercice 11</h1>
 
 <p>Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d’une chaîne de caractère représentant une date.</p>
 
@@ -8,8 +8,11 @@
 
 formaterDateFr("2018-02-23");
 
+
 function formaterDateFr($date){
-    
+    $date = new DateTimeImmutable($date);
+    $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE);
+    echo $formatter->format($date);
 }
 
 ?>
