3d17c7751f9e4a1587271043042efd241c9abab3
ErgoNoMister <strubcedric@gmail.fr>
2022-11-16 13:55:23 +0100

    Exo 7

---
+++ AlgoPHP/Partie 2/Exo8.php
@@ -0,0 +1,12 @@
+<h1>Exercice 8</h1>
+
+<p>Soit l’URL suivante: http://my.mobirise.com/data/userpic/764.jpg<br>
+Créer une fonction personnalisée permettant d’afficher l’image N fois à l’écran.</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+
+
+?>
